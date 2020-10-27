e327d37e4ecf56453449f6431115c6b165b9cdd3
pekarekmarek <73193774+pekarekmarek@users.noreply.github.com>
2020-10-27 13:36:12 +0100

    Update transmitter.ino

--- transmitter.ino
+++ transmitter.ino
@@ -1,10 +1,10 @@
 #include <SoftwareSerial.h>
-SoftwareSerial HC12(10, 11); // HC-12 TX Pin, HC-12 RX Pin
+SoftwareSerial HC12(10, 11); 
 //int pot = A2;
 int val;
 void setup() {
-  Serial.begin(9600);             // Serial port to computer
-  HC12.begin(9600);               // Serial port to HC12
+  Serial.begin(9600);           
+  HC12.begin(9600);               
   //pinMode(pot,INPUT);
 }
 void loop() {  
@@ -15,5 +15,5 @@ void loop() {
   HC12.write(val);
   delay(900);
   //int val = map(analogRead(pot),0,1024,0,255);
-       // Send that data to HC-12  
+       
 }
