0cd7abadeb8efaa4aae999cec32a24f062eadae0
pekarekmarek <73193774+pekarekmarek@users.noreply.github.com>
2020-10-27 13:35:28 +0100

    Update receiver.ino

--- receiver.ino
+++ receiver.ino
@@ -1,16 +1,16 @@
 #include <SoftwareSerial.h>
-SoftwareSerial HC12(10, 11); // HC-12 TX Pin, HC-12 RX Pin
+SoftwareSerial HC12(10, 11); 
 int LED = 3;
 int val;
 void setup() {
-  Serial.begin(9600);             // Serial port to computer
-  HC12.begin(9600);               // Serial port to HC12
+  Serial.begin(9600);             
+  HC12.begin(9600);               
   pinMode(LED,OUTPUT);
 }++
 void loop() {
-  while (HC12.available()) {        // If HC-12 has data
+  while (HC12.available()) {       
     val = HC12.read();
-    Serial.println(HC12.read());      // Send the data to Serial monitor
+    Serial.println(HC12.read());      
     //analogWrite(LED,val);*/
     digitalWrite(LED, val);
   }  
