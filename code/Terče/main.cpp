98c7e145d3c24b67def39be02bc36734e8d26d1b
pekarekmarek <m.pekarek.st@spseiostrava.cz>
2021-02-09 00:07:46 +0100

    indikace stavu baterie

--- code/Terče/main.cpp
+++ code/Terče/main.cpp
@@ -1,29 +1,39 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
-
-SoftwareSerial HC12(10, 11); // TX, RX
-
-#define pravy 4
+SoftwareSerial HC12(10, 11); // HC-12 TX Pin, HC-12 RX Pin
+#define batterypin A1
 #define levy 5
-int terc = 0;
+#define pravy 4
+byte terc = 0;
+int stav;
 
 void setup() {
-  Serial.begin(9600);            
-  HC12.begin(9600);   
-  digitalWrite(levy, HIGH);
-  digitalWrite(pravy, HIGH);
+  Serial.begin(115200);             // Serial port to computer
+  HC12.begin(115200);               // Serial port to HC12
+  pinMode(levy,INPUT_PULLUP);
+  pinMode(pravy,INPUT_PULLUP);
+  pinMode(batterypin, INPUT);
 }
 void loop() {  
-  terc = 5;
-  HC12.write(terc);
+  stav = analogRead(batterypin);
+  if (stav >= 820)  stav = 6;
+  else if (stav >= 780 && stav < 820) stav = 5; 
+  else if (stav >= 740 && stav < 780) stav = 4;
+  else if (stav >= 700 && stav < 740) stav = 3;
+  else if (stav >= 600 && stav < 700) stav = 2;
+  HC12.write(stav);
+  Serial.println(stav);
   if (digitalRead(levy) == LOW){
     terc = 1;
     HC12.write(terc);
+    Serial.println(terc);
   }
-  if (digitalRead(pravy) == LOW){
+  if (digitalRead(pravy) == LOW) {
     terc = 2;
     HC12.write(terc);
+    Serial.println(terc);
+    
   }
-  Serial.println(terc);
-   
+  terc = 0;
+  
 }
